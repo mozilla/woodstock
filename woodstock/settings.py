2e66e930b1b482863f4f2b07f3a0b2992ca7394b
Pierros Papadeas <ppapadeas@gmail.com>
2013-04-10 20:37:47 +0300

    Initil static templates

--- woodstock/settings.py
+++ woodstock/settings.py
@@ -111,6 +111,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    'templates',
 )
 
 INSTALLED_APPS = (
@@ -121,7 +122,9 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+
     'django.contrib.admin',
+    'woodstock.voting',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
--- woodstock/urls.py
+++ woodstock/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
+from django.views.generic import TemplateView
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -15,6 +16,8 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    (r'^$', TemplateView.as_view(template_name="index.html")),
+    (r'^dashboard/', TemplateView.as_view(template_name="dashboard.html")),
 )
 
 ## In DEBUG mode, serve media files through Django.
