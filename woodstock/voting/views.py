a2fb13098da0bd8d383249ab8b5c927caddce26f
Pierros Papadeas <ppapadeas@gmail.com>
2013-04-12 15:47:57 +0300

    Updated django-browserid to 0.8 and css fixes

--- woodstock/voting/views.py
+++ woodstock/voting/views.py
@@ -4,17 +4,22 @@ from django.shortcuts import redirect, render
 
 def main(request):
     """Main page view."""
-    return render(request, 'index.html')
+    if request.user.is_authenticated():
+        return redirect(dashboard)
+    else:
+        return render(request, 'index.html')
 
 
-#TODO check if user is authenticated
 def dashboard(request):
-    return render(request, 'dashboard.html')
+    if request.user.is_authenticated():
+        return render(request, 'dashboard.html')
+    else:
+        return redirect(main)
 
 
 def login_failed(request):
     """Redirect to login page on login failure."""
     messages.warning(request, ('Login failed. Please make sure that you '
-                               'have an account ,and your email '
+                               'have an account, and your email '
                                'is verified.'))
-    return redirect('main')
+    return render(request, 'index.html')
