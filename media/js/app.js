42dde18b644e21bdcb07628764cb88b55f826ece
Pierros Papadeas <ppapadeas@gmail.com>
2013-04-13 20:04:41 +0300

    Beautification of form and keystrokes implementation

--- media/js/app.js
+++ media/js/app.js
@@ -1,6 +1,47 @@
-$(document).ready(function() 
-    { 
-        $("#mozillians").tablesorter();
-        $('#shortcuts').tooltip();
-    } 
-); 
+$(document).ready(function() {
+    $("#mozillians").tablesorter();
+    $('#shortcuts').tooltip();
+
+    if ($('input[value="-1"]').is(':checked')) {
+        $('#button-no').addClass('disabled');
+    } else if ($('input[value="0"]').is(':checked')) {
+        $('#button-skip').addClass('disabled');
+    } else if ($('input[value="1"]').is(':checked')) {
+        $('#button-probably').addClass('disabled');
+    } else if ($('input[value="2"]').is(':checked')) {
+        $('#button-definitely').addClass('disabled');
+    }
+
+    $('#button-no').click(function() {
+        $('input[value="-1"]').attr('checked', true);
+        $('#voting_form').submit();
+    });
+    $('#button-skip').click(function() {
+        $('input[value="0"]').attr('checked', true);
+        $('#voting_form').submit();
+    });
+    $('#button-probably').click(function() {
+        $('input[value="1"]').attr('checked', true);
+        $('#voting_form').submit();
+    });
+    $('#button-definitely').click(function() {
+        $('input[value="2"]').attr('checked', true);
+        $('#voting_form').submit();
+    });
+
+    $(document).keydown( function(event) {
+        if (event.which == 88){
+            $('input[value="-1"]').attr('checked', true);
+            $('#voting_form').submit();
+        } else if (event.which == 48){
+            $('input[value="0"]').attr('checked', true);
+            $('#voting_form').submit();
+        } else if (event.which == 49){
+            $('input[value="1"]').attr('checked', true);
+            $('#voting_form').submit();
+        } else if (event.which == 50){
+            $('input[value="2"]').attr('checked', true);
+            $('#voting_form').submit();
+        }
+    });
+});
