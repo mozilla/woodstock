6b3122eb8598404ac8973ea1b4451775b16cc2f2
Tasos Katsoulas <akatsoulas@gmail.com>
2015-09-11 12:00:26 +0300

    Support csv import to new models.

--- woodstock/voting/admin.py
+++ woodstock/voting/admin.py
@@ -1,9 +1,49 @@
+import re
+
 from django.contrib import admin
+from django.core.validators import EmailValidator
+from django.forms import ValidationError
 
 from import_export.admin import ExportMixin, ImportExportMixin
 from import_export import fields, resources
 
-from woodstock.voting.models import MozillianGroup, MozillianProfile, Vote
+from woodstock.voting.models import (Application, Event, PreferredEvent,
+                                     MozillianGroup, MozillianProfile, Vote)
+
+
+RGX = re.compile('.+/u/(.+)/')
+
+
+class ApplicationResource(resources.ModelResource):
+
+    class Meta:
+        model = Application
+        fields = ('entry_id', 'number_of_events', 'event', 'reasoning',
+                  'contributions', 'learning_areas', 'recommendation_letter',
+                  'ideas', 'commitments', 'functional_team', 'team_contact',
+                  'participation_opportunities', 'tracking_communities',
+                  'community_record', 'community_impact', 'other', 'date',
+                  'application_complete',)
+        skip_unchanged = True
+        report_skipped = True
+
+
+class EventResource(resources.ModelResource):
+
+    class Meta:
+        model = Event
+        fields = ('name',)
+        skip_unchanged = True
+        report_skipped = True
+
+
+class PreferredEventResource(resources.ModelResource):
+
+    class Meta:
+        model = PreferredEvent
+        fields = ('event', 'application', 'preferred',)
+        skip_unchanged = True
+        report_skipped = True
 
 
 class MozillianGroupResouce(resources.ModelResource):
@@ -15,6 +55,10 @@ class MozillianGroupResouce(resources.ModelResource):
 
     class Meta:
         model = MozillianProfile
+        skip_unchanged = True
+        report_skipped = True
+        import_id_fields = ('mozillian_username',)
+        fields = ('mozillian_username', 'email',)
 
     def dehydrate_negative_votes(self, mozillianprofile):
         return mozillianprofile.votes.filter(vote=-1).count()
@@ -33,6 +77,53 @@ class MozillianGroupResouce(resources.ModelResource):
         positives = (mozillianprofile.votes.filter(vote=1).count() +
                      mozillianprofile.votes.filter(vote=2).count())
         return (positives - negatives)
+
+    # Import section
+    def before_import(self, dataset, dry_run, **kwargs):
+        """Override method for custom functionality."""
+
+        # Normalize dataset
+        if dataset.headers:
+            dataset.headers = [unicode(header).lower().strip()
+                               for header in dataset.headers]
+
+    def get_or_init_instance(self, instance_loader, row):
+        """Override method for custom functionality."""
+
+        # If ther isn't a username or the application is not complete,
+        # do not return an instance.
+        if ('mozillian_username' not in row or
+            'application_complete' not in row or
+                row['application_complete'] == u'0'):
+            row['mozillian_username'] = ''
+
+        # Normalize usernames
+        match = RGX.match(row['mozillian_username'])
+        if match:
+            row['mozillian_username'] = match.groups()[0]
+
+        # Validate email
+        email_validator = EmailValidator()
+        try:
+            email_validator(row['email'])
+        except ValidationError:
+            row['email'] = ''
+
+        return (super(MozillianGroupResouce, self)
+                .get_or_init_instance(instance_loader, row))
+
+    def skip_row(self, instance, original):
+        if instance and (instance.mozillian_username == '' or
+                         instance.email == ''):
+            return True
+        return super(MozillianGroupResouce, self).skip_row(instance, original)
+
+    def save_instance(self, instance, dry_run):
+        if not dry_run:
+            # TODO: Get the rest of the info from mozillians.org
+            # Probably we need to check if there are any changes to the profile
+            # and update it
+            super(MozillianGroupResouce, self).save_instance(instance, dry_run)
 
 
 class MozillianProfileAdmin(ImportExportMixin, admin.ModelAdmin):
@@ -78,6 +169,24 @@ class MozillianGroupAdmin(ExportMixin, admin.ModelAdmin):
     model = MozillianGroup
 
 
+class ApplicationAdmin(ImportExportMixin, admin.ModelAdmin):
+    resource_class = ApplicationResource
+    model = Application
+
+
+class EventAdmin(ImportExportMixin, admin.ModelAdmin):
+    resource_class = EventResource
+    model = Event
+
+
+class PreferredEventAdmin(ImportExportMixin, admin.ModelAdmin):
+    resource_class = PreferredEventResource
+    model = PreferredEvent
+
+
 admin.site.register(MozillianGroup, MozillianGroupAdmin)
 admin.site.register(Vote, VoteAdmin)
 admin.site.register(MozillianProfile, MozillianProfileAdmin)
+admin.site.register(Application, ApplicationAdmin)
+admin.site.register(Event, EventAdmin)
+admin.site.register(PreferredEvent, PreferredEventAdmin)
